a42a44c33e6e6b6a372bd6c2ace826ec1b781856
denis ekimenko <den.pdb@gmail.com>
2023-02-19 21:57:01 +0300

    edit brain_games/cli.py auto-edit brain_games/__pycache__/cli.cpython-310.pyc

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -4,3 +4,14 @@ import prompt
 def welcome_user():
     name = prompt.string('May I have your name????')
     print(f'hello, {name}!')
+
+def even(number):
+        answer = prompt.string(f'Qestion: {number} ')
+        return answer
+
+def correct():
+      print('Correct!')
+
+
+def wrong():
+      print('Wrong')
