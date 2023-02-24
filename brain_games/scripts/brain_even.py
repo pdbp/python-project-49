fbde8384068ebabf91bc880e69da0dca587fcb51
denis ekimenko <den.pdb@gmail.com>
2023-02-24 22:57:54 +0300

    edit brain_games/scripts/brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,25 +1,11 @@
 #!/usr/bin/env python3
 from random import randint
 from .. import cli
+from .. import logic
 
 
 def main():
-    print("Welcome to the Brain Games!")
-    name = cli.welcome_user()
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    correct_answers = 0
-    working = True
-    while working:
-        answer = even()
-        if answer:
-            correct_answers += 1
-            if correct_answers == 3:
-                print(f"Congratulations, {name}!")
-                working = False
-        else:
-            correct_answers = 0
-            print(f"Let's try again, {name}!")
-            working = False
+    logic.main('brain-even')
 
 
 def even():
@@ -28,13 +14,12 @@ def even():
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
-    answer = cli.even(number)
+    answer = cli.question(number)
     if answer == correct_answer:
         print('Correct!')
         return True
     else:
-        print(f'''
-'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.''')
+        print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
         return False
 
 
