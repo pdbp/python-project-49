d4a44efa8d9a2c31cf4a871c9b1837404601f176
denis ekimenko <den.pdb@gmail.com>
2023-02-26 15:24:46 +0300

    fix: refactoring brain_games/games/prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,14 +4,7 @@ from .. import cli
 
 def game():
     number = randint(1, 100)
-    r_answ = 'yes'
-    if number > 1:
-        for num in range(2, number):
-            if number % num == 0:
-                r_answ = 'no'
-                break
-    else:
-        r_answ = 'no'
+    r_answ = is_prime(number)
     answ = cli.question(number)
     if answ == r_answ:
         print('Correct!')
@@ -19,3 +12,12 @@ def game():
     else:
         print(f"'{answ}' is wrong answer ;(. Correct answer was '{r_answ}'.")
         return False
+
+
+def is_prime(number):
+    if number > 1:
+        for num in range(2, number):
+            if number % num == 0:
+                return 'no'
+        return 'yes'
+    return 'no'
