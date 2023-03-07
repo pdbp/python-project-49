1ba9bf8ec127b2ea019280dd2928859830e5f986
denis ekimenko <den.pdb@gmail.com>
2023-03-07 23:46:00 +0300

    fix: refactoring brain_games/logic.py

--- brain_games/logic.py
+++ brain_games/logic.py
@@ -5,22 +5,15 @@ def main(game):
     print("Welcome to the Brain Games!")
     name = cli.welcome_user()
     print(game.rules())
-    question, r_ans = game.game()
-    if is_win(question, r_ans):
-        print(f"Congratulations, {name}!")
-    else:
-        print(f"Let's try again, {name}!")
-
-
-def is_win(question, r_ans):
     score = 0
     while score < 3:
+        question, r_ans = game.game()
         ans = cli.question(question)
         if ans == r_ans:
             print('Correct!')
             score += 1
         else:
             print(f"'{ans}' is wrong answer ;(. Correct answer was '{r_ans}'.")
-            return False
-    if score == 3:
-        return True
+            print(f"Let's try again, {name}!")
+            return 0
+    print(f"Congratulations, {name}!")
