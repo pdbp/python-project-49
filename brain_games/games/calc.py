8f8b4a84946ac06c4e8946fd91d7150df8b8bd51
denis ekimenko <den.pdb@gmail.com>
2023-03-10 00:05:51 +0300

    fix: to put the logic for calculating the answer in a separate function

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -10,13 +10,18 @@ def game():
     RAND_SIGNS_RANGE = ('+', '-', '*')
     sign = choice(RAND_SIGNS_RANGE)
     expression = f'{str(number1)} {choice(sign)} {str(number2)}'
+    r_answ = get_right_answer(number1, number2, sign)
+    return expression, r_answ
+
+
+def get_right_answer(number1, number2, sign):
     if sign == '+':
         r_answ = str(number1 + number2)
     elif sign == '-':
         r_answ = str(number1 - number2)
     else:
         r_answ = str(number1 * number2)
-    return expression, r_answ
+    return r_answ
 
 
 def rules():
--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -5,11 +5,17 @@ def game():
     START_RAND_NUMB_RANGE = 1
     END_RAND_NUMB_RANGE = 100
     number = randint(START_RAND_NUMB_RANGE, END_RAND_NUMB_RANGE)
-    if (number % 2) == 0:
-        r_answ = 'yes'
-    else:
-        r_answ = 'no'
+    answer_converter = {True: 'yes', False: 'no'}
+    r_answ = answer_converter[is_even(number)]
     return number, r_answ
+
+
+def is_even(number):
+    if (number % 2) == 0:
+        r_answ = True
+    else:
+        r_answ = False
+    return r_answ
 
 
 def rules():
